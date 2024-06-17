6d240832778f9d0ef8f0253301911d601f0fd74b
alex-v08 <alex.velasquez08@outlook.com>
2024-06-17 12:51:21 -0300

    Add database configuration check

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	
+
+	"github.com/alex-v08/inventory-fw/database"
+	"github.com/alex-v08/inventory-fw/settings"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/fx"
+)
+
+func main() {
+
+	app := fx.New(
+		fx.Provide(
+			context.Background,
+			settings.New,
+			database.New,
+		),
+		
+		fx.Invoke(
+			func(db *sqlx.DB){
+				_, err := db.Query("SELECT * FROM USERS")
+				if err != nil {
+					panic(err)
+
+			}
+		},
+			
+		),
+	)
+
+	app.Run()
+
+}
